4e6bc9eccf47db2661c5315fe7282f6f4ca6ba91
piotrlewoniec <117780551+piotrlewoniec@users.noreply.github.com>
2023-07-30 16:29:20 +0200

    backup

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,15 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { axiosData } from 'js/apireset/axios-data';
-
-axios.defaults.baseURL =
-  'https://64c560bfc853c26efadac8fe.mockapi.io/api/pml/v1/';
+import { headerDefaultGet, headerDeafultURL } from 'js/config/stdquery';
 
 export const getContacts = createAsyncThunk(
   'fetchContacts/request',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/contacts');
+      const header = { ...headerDefaultGet, ...headerDeafultURL };
+      header.url = '/contacts';
+      const response = await axiosData(header);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
